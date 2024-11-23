cc26d22dc0e69dbd406ff8c43c34b2800c0ebf1a
Thomas Turner <thomas_turner@talktalk.net>
2024-11-23 14:33:05 +0000

    a21snsxx: Remove overlays
    * All of the overlays in the a21s-common are common, so we will
    * define all of our overlays in the common tree.

--- device.mk
+++ device.mk
@@ -22,6 +22,3 @@ $(call inherit-product, vendor/samsung/a21snsxx/a21snsxx-vendor.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
